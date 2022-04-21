bf30092cbbdcf64c642e9b0728bb3af5cecc9192
aruna <aruna.kumara@wearenoetic.com>
2022-04-21 23:51:15 +0530

    add find digit challenge

--- src/main/java/HackerRankFindDigits.java
+++ src/main/java/HackerRankFindDigits.java
@@ -1,11 +1,24 @@
 public class HackerRankFindDigits {
 
     public static int findDigits(int n) {
+        String s = String.valueOf(n);
+        int indexCount = 0;
         // Write your code here
-     return 1;
+        for (int x =0; x < s.length(); x++) {
+            int p = Integer.parseInt(Character.toString(s.charAt(x)));
+            if (p == 0) {
+                continue;
+            } else {
+                int y = n%p;
+                if (y == 0){
+                    indexCount++;
+                }
+            }
+        }
+     return indexCount;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(findDigits(124));
     }
 }
