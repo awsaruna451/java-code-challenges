37e10b26ae9caac71f43608d2f19e007a6de31c0
aruna <aruna.kumara@wearenoetic.com>
2022-04-21 23:19:59 +0530

    commit F

---
+++ src/main/java/HackerRankFindDigits.java
@@ -0,0 +1,11 @@
+public class HackerRankFindDigits {
+
+    public static int findDigits(int n) {
+        // Write your code here
+     return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
